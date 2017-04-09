0d9eb71551cf5529d13bb6c7ddf770032d118584
Ignatius van Niekerk <ignatius.van-niekerk@blackswan.com>
2017-04-09 17:23:57 +0200

    l

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {LoadersCssModule} from 'angular2-loaders-css';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    LoadersCssModule
   ],
   providers: [],
   bootstrap: [AppComponent]
